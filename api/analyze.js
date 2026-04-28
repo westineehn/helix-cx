061786cce75206f127afa24e0ab156e8af69fc77
westineehn <westineehn@gmail.com>
2026-04-28 16:25:26 -0700

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,3 @@
-export const config = { runtime: 'edge' };
-
 export default async function handler(req) {
   if (req.method !== 'POST') {
     return new Response('Method not allowed', { status: 405 });
